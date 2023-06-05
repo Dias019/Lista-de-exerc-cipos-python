fccd2cd32776c5dee0140f104efb6c0943da164a
Gabriel Leonardo Dias <116768323+Dias019@users.noreply.github.com>
2023-06-05 11:05:37 -0300

    Add files via upload

---
+++ soma.py
@@ -0,0 +1,7 @@
+#Crie um programa que leia dois números e mostre a soma entre eles
+
+numreo1 = float(input('Entre com o primeiro número: '))
+numreo2 = float(input('Entre com o segundo número: '))
+resultado = numreo1 + numreo2
+
+print('Resultado da soma = ',resultado)
